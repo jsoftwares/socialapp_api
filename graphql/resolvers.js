e0ddab711359322b63c9c075aa42d61b1fd9a0cf
Jeffrey Onochie <jeffreyonochie@gmail.com>
2021-01-12 12:05:06 +0100

    Add user Signup mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,26 @@
+const bcrypt = require('bcryptjs');
+
+
+const User = require('../models/user');
+
 module.exports = {
-	hello: () => {
-		return {
-			text: 'Hello Jeffrey Onochie',
-			views: 12345
+	createUser: async function(args, req){
+		const existingUser = await User.findOne({email: args.userInput.email});
+		if (existingUser) {
+			const error = new Error('Email already taken.');
+			throw error;
 		}
+
+		const hashedPw = await bcrypt.hash(args.userInput.password, 12);
+		const user = new User({
+			name: args.userInput.name.toUpperCase(),
+			email: args.userInput.email.toLowerCase(),
+			password: hashedPw
+		});
+
+		const createdUser = await user.save();
+
+		return {...createdUser._doc, _id:createdUser._id.toString()};
+
 	}
 }
